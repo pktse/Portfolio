fb2497c87cf4d19a2d523cd6e8b815146b6ed9cf
Patricia Tse <patriciaktse@berkeley.edu>
2021-03-17 17:47:27 -0700

    added sotira

--- js/index-script.js
+++ js/index-script.js
@@ -1,46 +1,42 @@
 //<script src = "https://ajax.googleapis.com/ajax/ibs/jquery/3.1.0/jquery.min.js"></script>
-var words = [ "a sophomore at UC Berkeley", "San Franciscan", "foodie", "guitarist", "pianist", "pun-lover", "artist", "computer scientist" ];
+var words = [ "a sophomore at UC Berkeley", "San Franciscan", "foodie", "pun-lover", "computer scientist" ];
 
 var i = 0;
-var i2 = 0;
-var _INTERVAL_VAL;
-var _ELEMENT = document.querySelector("#text");
+var j = 0;
+var interval;
+var elem = document.querySelector("#text");
 
 // Implements typing effect
 function Type() {
-  var text =  words[i].substring(0, i2 + 1);
-  _ELEMENT.innerHTML = text;
-  i2++;
+  var text =  words[i].substring(0, j + 1);
+  elem.innerHTML = text;
+  j++;
 
   // If full sentence has been displayed then start to delete the sentence after some time
   if(text === words[i]) {
-      clearInterval(_INTERVAL_VAL);
-      setTimeout(function() {
-          _INTERVAL_VAL = setInterval(Delete, 50);
-      }, 1000);
+      clearInterval(interval);
+      setTimeout(function() { interval = setInterval(Delete, 50); }, 1000);
   }
 }
 
 // Implements deleting effect
 function Delete() {
-  var text =  words[i].substring(0, i2 - 1);
-  _ELEMENT.innerHTML = text;
-  i2--;
+  var text =  words[i].substring(0, j - 1);
+  elem.innerHTML = text;
+  j--;
 
   // If sentence has been deleted then start to display the next sentence
   if(text === '') {
-      clearInterval(_INTERVAL_VAL);
+      clearInterval(interval);
 
-      if(i == (words.length - 1))
+      if (i == (words.length - 1))
           i = 0;
       else
           i++;
-      i2 = 0;
+      j = 0;
 
-      setTimeout(function() {
-          _INTERVAL_VAL = setInterval(Type, 100);
-      }, 200);
+      setTimeout(function() { interval = setInterval(Type, 100); }, 200);
   }
 }
 
-_INTERVAL_VAL = setInterval(Type, 100);
+interval = setInterval(Type, 100);
